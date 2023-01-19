c3a34c6a201e303b289335abeae961ae0f88182b
Zach Peterson <zachpeterson13@icloud.com>
2023-01-19 10:59:06 -0700

    Formatter test

--- src/factjunk_app.erl
+++ src/factjunk_app.erl
@@ -4,38 +4,43 @@
 %%%-------------------------------------------------------------------
 
 -module(factjunk_app).
+
 -include_lib("eunit/include/eunit.hrl").
+
 -behaviour(application).
+
 -export([start/2, stop/1, fac/1, rpc/2, loop/0, start/0]).
 
-fac(0) -> 1;
-fac(N) -> N * fac(N-1).
+fac(0) ->
+  1;
+fac(N) ->
+  N * fac(N - 1).
 
 start(_StartType, _StartArgs) ->
-    factjunk_sup:start_link().
+  factjunk_sup:start_link().
 
 start() ->
-    spawn(?MODULE, loop, []).
+  spawn(?MODULE, loop, []).
 
 loop() ->
-    receive
-        {Pid, Num} when is_integer(Num) == false; Num < 0 ->
-            Pid ! {fail, -1},
-            loop();
-        {Pid, Num} ->
-            Prod = fac(Num),
-            Pid ! {ok, Prod},
-            loop()
-    end.
+  receive
+    {Pid, Num} when is_integer(Num) == false; Num < 0 ->
+      Pid ! {fail, -1},
+      loop();
+    {Pid, Num} ->
+      Prod = fac(Num),
+      Pid ! {ok, Prod},
+      loop()
+  end.
 
 rpc(Pid, Num) ->
-    Pid ! {self(), Num},
-    receive
-        {ok, Result} ->
-            Result;
-        {fail, _} ->
-            fail
-    end.
+  Pid ! {self(), Num},
+  receive
+    {ok, Result} ->
+      Result;
+    {fail, _} ->
+      fail
+  end.
 
 stop(_State) ->
-    ok.
+  ok.
--- test/fact_tests.erl
+++ test/fact_tests.erl
@@ -1,19 +1,16 @@
 -module(fact_tests).
+
 -include_lib("eunit/include/eunit.hrl").
 
 fac_test_() ->
-    [
-    ?_assertEqual(1, factjunk_app:fac(0)),
-    ?_assertEqual(1, factjunk_app:fac(1)),
-    ?_assertEqual(720, factjunk_app:fac(6))
-    ].
+  [?_assertEqual(1, factjunk_app:fac(0)),
+   ?_assertEqual(1, factjunk_app:fac(1)),
+   ?_assertEqual(720, factjunk_app:fac(6))].
 
 rpc_test_() ->
-    Pid = factjunk_app:start(),
-    [
-    ?_assertEqual(fail, factjunk_app:rpc(Pid, -1)),
-    ?_assertEqual(fail, factjunk_app:rpc(Pid, 1.5)),
-    ?_assertEqual(1, factjunk_app:rpc(Pid, 0)),
-    ?_assertEqual(1, factjunk_app:rpc(Pid, 1)),
-    ?_assertEqual(720, factjunk_app:rpc(Pid, 6))
-    ].
+  Pid = factjunk_app:start(),
+  [?_assertEqual(fail, factjunk_app:rpc(Pid, -1)),
+   ?_assertEqual(fail, factjunk_app:rpc(Pid, 1.5)),
+   ?_assertEqual(1, factjunk_app:rpc(Pid, 0)),
+   ?_assertEqual(1, factjunk_app:rpc(Pid, 1)),
+   ?_assertEqual(720, factjunk_app:rpc(Pid, 6))].
